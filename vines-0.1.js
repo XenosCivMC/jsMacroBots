2011b516dcc131452bd0cdf2fed16837e520bfaa
Xenos <xenoscivmc@gmail.com>
2025-09-17 09:12:03 +0200

    dev vines

--- vines-0.1.js
+++ vines-0.1.js
@@ -1,5 +1,5 @@
 const botname = "XenosVinesBot";
-const farmLength = 101;
+const farmLength = 32;
 // let farmLength = 5;
 // const farmRows = 2;
 
@@ -17,7 +17,7 @@ function gotoCenter() {
   const pitch = player.getPitch();
   let currentPos = player.getPos();
   const currentCenterPos = getCenterPosition(currentPos);
-  while ((getDistance(currentPos, currentCenterPos) > 0.3) && isRunning(botname)) {
+  while ((getDistance(currentPos, currentCenterPos) > 0.2) && isRunning(botname)) {
     player.lookAt(currentCenterPos.x, currentCenterPos.y, currentCenterPos.z);
     KeyBind.keyBind('key.sneak', true);
     KeyBind.keyBind('key.forward', true);
@@ -29,25 +29,56 @@ function gotoCenter() {
   KeyBind.keyBind('key.sneak', false);
   KeyBind.keyBind('key.forward', false);
   player.lookAt(yaw, pitch);
-  Client.waitTick(1);
+  Client.waitTick(20);
 }
 
-function strafeToPosition(pos) {
+function strafeToPosition(pos, dir) {
+  Chat.log(`Strafing to: ${pos}`);
+
+  // where is the player leaning towards?
+  let leaning = "";
+  if (dir.primaryDir.x != 0 && dir.primaryDir.x == dir.secondaryDir.z) leaning = "right";
+  if (dir.primaryDir.x != 0 && dir.primaryDir.x != dir.secondaryDir.z) leaning = "left";
+  if (dir.primaryDir.z != 0 && dir.primaryDir.z != dir.secondaryDir.x) leaning = "right";
+  if (dir.primaryDir.z != 0 && dir.primaryDir.z == dir.secondaryDir.x) leaning = "left";
+
 
   const player = Player.getPlayer();
   let currentPos = player.getPos();
-  const dir = getPlayerDirection();
-  // walk to the tree while swinging
-  while ((getDistance(currentPos, pos) > 0.3) && isRunning(botname)) {
+  while ((getDistance(currentPos, pos) > 0.4) && isRunning(botname)) {
+    Chat.log(getDistance(currentPos, pos));
     player.lookAt(currentPos.x + dir.primaryDir.x, currentPos.y + 1.6, currentPos.z + dir.primaryDir.z);
-    KeyBind.keyBind('key.left', true);
+    KeyBind.keyBind(`key.${leaning}`, true);
 
     Client.waitTick(1);
     if (!isRunning(botname))
       return;
     currentPos = player.getPos();
   }
-  KeyBind.keyBind('key.left', false);
+  KeyBind.keyBind(`key.${leaning}`, false);
+}
+
+function climbLadder(dir) {
+
+  // where is the player leaning towards?
+  let leaning = "";
+  if (dir.primaryDir.x != 0 && dir.primaryDir.x == dir.secondaryDir.z) leaning = "right";
+  if (dir.primaryDir.x != 0 && dir.primaryDir.x != dir.secondaryDir.z) leaning = "left";
+  if (dir.primaryDir.z != 0 && dir.primaryDir.z != dir.secondaryDir.x) leaning = "right";
+  if (dir.primaryDir.z != 0 && dir.primaryDir.z == dir.secondaryDir.x) leaning = "left";
+
+  let leaningOther = "";
+  if (leaning == "right") leaningOther = "left";
+
+  gotoCenter();
+  KeyBind.keyBind(`key.${leaning}`, true);
+
+  Client.waitTick(50);
+  KeyBind.keyBind(`key.${leaning}`, false);
+  KeyBind.keyBind(`key.${leaningOther}`, true);
+  Client.waitTick(15);
+  KeyBind.keyBind(`key.${leaningOther}`, false);
+
 }
 
 function doVineColumn(dir) {
@@ -59,21 +90,23 @@ function doVineColumn(dir) {
   player.lookAt(currentYaw, 27);
 
   KeyBind.key("key.mouse.left", true);
-  Client.waitTick(40);
+  Client.waitTick(50);
   KeyBind.key("key.mouse.left", false);
 }
 
 function doVineRow(dir) {
   const player = Player.getPlayer();
   gotoCenter();
+  let currentPos = player.getPos();
+  player.lookAt(currentPos.x + dir.primaryDir.x, currentPos.y + 1.6, currentPos.z + dir.primaryDir.z);
   doVineColumn(dir);
-  for (let i = 0; i < farmLength && isRunning(botname); i++) {
-    const currentPos = player.getPos();
+  for (let i = 0; i < farmLength - 1 && isRunning(botname); i++) {
+    currentPos = player.getPos();
     const currentCenterPos = getCenterPosition(currentPos);
     const goalPos = { ...currentCenterPos };
     goalPos.x += dir.secondaryDir.x;
     goalPos.z += dir.secondaryDir.z;
-    strafeToPosition(goalPos);
+    strafeToPosition(goalPos, dir);
     doVineColumn(dir);
   }
 }
@@ -86,8 +119,9 @@ toggleGlobalVar(botname);
 if (isRunning(botname)) {
   // const player = Player.getPlayer();
   const dir = getPlayerDirection();
-  doVineRow(dir);
   // gotoCenter();
+  // doVineRow(dir);
+  climbLadder(dir);
   GlobalVars.putBoolean(botname, false);
   Client.waitTick(1);
   cleanup(botname);
